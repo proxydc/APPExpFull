bad4d252d7b70297c4d6cc553185390c0717c06b
Nithya SEETHARAMAN <a.seetharaman@gmail.com>
2024-02-25 21:23:49 +0100

    sql

---
+++ test/main.py
@@ -0,0 +1,17 @@
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+
+
+class We_Box(BoxLayout):
+    pass
+
+
+class We_Grid(GridLayout):
+    pass
+
+
+class We_Main(App):
+    pass
+
+We_Main().run()
